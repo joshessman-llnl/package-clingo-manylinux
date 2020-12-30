db47900923a18e11ad73a24592cde4f9a7ef1766
Josh Essman <essman1@llnl.gov>
2020-12-30 09:44:26 -0600

    fix: python hardcoded

--- build.sh
+++ build.sh
@@ -14,7 +14,16 @@ function repair_wheel {
 for PYBIN in /opt/python/*/bin; do
     # Requires Py3.6 or greater - on the docker image 3.5 is cp35-cp35m
     if ! [[ ${PYBIN} =~ 35 ]] ; then
-        export Python_ROOT_DIR="${PYBIN}/.."
+        export Python_ROOT_DIR=${PYBIN}/..
+        export Python_INCLUDE_DIR=${PYBIN}/../include/python*
+        export Python_LIBRARY=${PYBIN}/../lib/python*/libpython*.so
+        export Python_EXECUTABLE=${PYBIN}/python
+        echo $Python_INCLUDE_DIR
+        file $Python_INCLUDE_DIR
+        echo $Python_LIBRARY
+        file $Python_LIBRARY
+        echo $Python_EXECUTABLE
+        file $Python_EXECUTABLE
         "${PYBIN}/pip" wheel ./clingo/ --no-deps -w wheelhouse/
     fi
 done
