6989f1ad902b68281792b142787a8e0a126061bd
Josh Essman <essman1@llnl.gov>
2020-12-30 08:42:28 -0600

    feat: add script to actually build wheels

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -u -x
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w wheelhouse/
+    fi
+}
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel clingo --no-deps -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f wheelhouse/
+    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
