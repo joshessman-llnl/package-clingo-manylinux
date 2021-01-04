cd5fc5f120e1bef4a1f39fe345ce8938a47ded68
Josh Essman <essman1@llnl.gov>
2021-01-04 15:50:00 -0600

    feat: attempt to push to testpypi

--- build.sh
+++ build.sh
@@ -10,6 +10,18 @@ function repair_wheel {
     fi
 }
 
+# Temporary, for testing purposes only
+function rename_wheel {
+    interp="$1"
+    wheel="$2"
+    $interp -m wheel unpack --dest rename_wheel "$wheel"
+    rm "$wheel"
+    sed -i 's/Name: clingo-cffi/Name: clingo-cffi-wheel/g' rename_wheel/clingo_cffi*/clingo_cffi*/METADATA
+    $interp -m wheel pack rename_wheel/clingo_cffi*
+    mv "$wheel" "$(echo $wheel | sed 's/clingo_cffi/clingo_cffi_wheel/g')"
+    rm -rf rename_wheel
+}
+
 cd clingo && git apply ../dynamic_lookup.patch && cd ..
 
 # Compile wheels
@@ -17,6 +29,8 @@ for PYBIN in /opt/python/*/bin; do
     # Requires Py3.6 or greater - on the docker image 3.5 is cp35-cp35m
     if ! [[ ${PYBIN} =~ 35 ]] ; then
         "${PYBIN}/pip" wheel ./clingo/ --no-deps -w wheelhouse/
+        just_built_wheel=$(ls -Art wheelhouse/ | tail -n 1)
+        rename_wheel "${PYBIN}/python" "$just_built_wheel"
     fi
 done
 
